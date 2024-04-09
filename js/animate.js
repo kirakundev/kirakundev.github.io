d1b9bd3e1c2bf6ad1f2ef505166889a0b2ca7488
Kira Dev <83586926+kirakundev@users.noreply.github.com>
2024-04-09 19:36:08 +0000

    s

---
+++ js/animate.js
@@ -0,0 +1,69 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Assalamu'alaikum Warrahmatullahi Wabarakatuh,\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1443 Hijriah, saudaraku.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Saya Nanda, di hari yang suci dan penuh berkah ini, apabila saya pernah berbuat salah baik dari perilaku maupun lisan\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "maka ingatlah bahwa itu semua... MEMANGLAH SAYA SENGAJA\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi ingin meminta maaf sebesar-besarnya atas segala kekhilafan saya.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 350
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 350
+        },
+        {
+            text: "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n",
+            delayAfter: 350
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
